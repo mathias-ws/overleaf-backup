1853cb8a2a197602ffb1e4adfaebce52545283f0
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2025-01-08 19:29:07 +0100

    fix(gitcreds): Using absolute path instead of relative path

--- overleaf_backup/backup.py
+++ overleaf_backup/backup.py
@@ -141,10 +141,7 @@ class OverleafRepo:
                 self.__config.gitlab.access_token.get_secret_value()
             )
 
-        if os.getenv("DOCKER_RUNTIME") == "1":
-            os.environ["GIT_ASKPASS"] = "../assets/git_creds.sh"
-        else:
-            os.environ["GIT_ASKPASS"] = "assets/git_creds.sh"
+        os.environ["GIT_ASKPASS"] = f"{os.getcwd()}/assets/git_creds.sh"
 
         result = subprocess.run(
             command,
