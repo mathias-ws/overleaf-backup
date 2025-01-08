e2815c97499324964b57f0e7656e07103dd865d9
Mathias Westby Skoglund <mail@mathiaswskoglund.com>
2025-01-08 19:30:08 +0100

    fix(gitcreds): Providing username and password only when asked

--- assets/git_creds.sh
+++ assets/git_creds.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-[ -n "$GIT_USERNAME" ] && echo "$GIT_USERNAME"
-echo $GIT_PASSWORD
+# Credits to https://stackoverflow.com/a/68358639 for the switch case solution to this problem.
+case "$1" in
+    Username*) exec echo "$GIT_USERNAME" ;;
+    Password*) exec echo "$GIT_PASSWORD" ;;
+esac
